d0749a54046dbf15f8c180c31ed5072d34736b1e
Percide07 <percidehonhonou@gmail.com>
2023-12-04 13:29:52 +0100

    create a routing system to access to differents pages or elements

--- src/modules/Form/index.jsx
+++ src/modules/Form/index.jsx
@@ -1,7 +1,9 @@
 import Input from "../../components/inputs/index";
 import Button from "../../components/Button/index";
 import { useState } from "react";
-const Form = ({
+import { useNavigate } from "react-router-dom";
+
+const   Form = ({
     isSignInPage = true,
 }) => {
     const [data,setData] = useState({
@@ -11,21 +13,26 @@ const Form = ({
         email:'',
         password:'',
     })
-    console.log('data', data)
+    // console.log('data', data)
+    const navigate = useNavigate()
     return (
-        <div className="bg-blue-300 h-[600px] w-[600px] flex flex-col items-center justify-center shadow-lg rounded">
-            <div className="text-4xl font-extrabold">Welcome {isSignInPage && 'Back'} </div>
-            <div className=" text-xl font-light mb-10">{isSignInPage ? 'Sign in to get explored' : 'Sign up to get started' } </div>
-            <form className="flex flex-col items-center w-full" onSubmit={() => {}}>
-                {!isSignInPage && <Input label='Full Name' name='name' placeholder='Enter your full name' className='mb-8 ' value={data.fullName} 
-                onChange={(e) => setData({...data, fullName: e.target.value})}/>}
-                <Input label='Email Address' type="email" name='name' placeholder='Enter your email' className='mb-8' value={data.email}
-                onChange={(e) => setData({...data, email: e.target.value})}/>
-                <Input label='Password' type="password" name='Password' placeholder='Enter your Password' className='mb-8' value={data.password} 
-                onChange={(e) => setData({...data, password: e.target.value})}/>
-                <Button label={isSignInPage?'Sign up':'Sign up'} className='mt-8 mb-4 w-1/2' type="submit"/>
-            </form>
-            <div>{isSignInPage?"Didn't have an account ?":'Already have an account?'} <span className="text-primary cursor-pointer underline ">{isSignInPage?'Sign up':'Sign in'}</span></div>
+        <div className="bg-light h-screen flex items-center justify-center">
+            <div className="bg-blue-300 h-[600px] w-[600px] flex flex-col items-center justify-center shadow-lg rounded">
+                <div className="text-4xl font-extrabold">Welcome {isSignInPage && 'Back'} </div>
+                <div className=" text-xl font-light mb-10">{isSignInPage ? 'Sign in to get explored' : 'Sign up to get started' } </div>
+                <form className="flex flex-col items-center w-full" onSubmit={() => {}}>
+                    {!isSignInPage && <Input label='Full Name' name='name' placeholder='Enter your full name' className='mb-8 w-[50%]' value={data.fullName} 
+                    onChange={(e) => setData({...data, fullName: e.target.value})}/>}
+                    <Input label='Email Address' type="email" name='name' placeholder='Enter your email' className='mb-8 w-[50%]' value={data.email}
+                    onChange={(e) => setData({...data, email: e.target.value})}/>
+                    <Input label='Password' type="password" name='Password' placeholder='Enter your Password' className='mb-8 w-[50%]' value={data.password} 
+                    onChange={(e) => setData({...data, password: e.target.value})}/>
+                    <Button label={isSignInPage?'Sign up':'Sign up'} className='mt-8 mb-4 w-1/2' type="submit"/>
+                </form>
+                <div>{isSignInPage?"Didn't have an account ?":'Already have an account?'} <span className="text-primary cursor-pointer underline"
+                onClick={()=>{navigate(`/users/${isSignInPage ? 'sign_up' : 'sign_in'} `)} }>
+                    {isSignInPage?'Sign up':'Sign in'}</span></div>
+            </div>
         </div>
     )
 }
